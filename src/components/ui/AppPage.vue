01354cca42a5f1ed86960d56bf127aa4e5e602dd
Заур <zaurmag411@gmail.com>
2021-05-27 15:36:10 +0300

    + Init project + Удалил лишние компоненты Vue + Перенес html разметку

---
+++ src/components/ui/AppPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page">
+    <div class="container">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  name: 'AppPage',
+  setup (props) {
+    const title = ref(props.title)
+    document.title = title.value
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import './main.min.css'
 
-createApp(App).use(store).mount('#app')
+createApp(App)
+  .use(store)
+  .mount('#app')
